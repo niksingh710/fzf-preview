5f6e936d1c3943192f9bdade71cff8879bdab3a6
nikhil.singh <nik.singh710@gmail.com>
2025-06-26 18:03:40 +0530

    udpate: general syntax fixes

--- default.nix
+++ default.nix
@@ -2,13 +2,32 @@
 
 pkgs.writeShellApplication {
   name = "fzf-preview";
-  runtimeInputs = with pkgs;[ file jq bat glow w3m eza openssl atool p7zip libcdio odt2txt catdoc gnumeric exiftool chafa mediainfo ffmpegthumbnailer poppler_utils ];
+  runtimeInputs = with pkgs;[
+    file
+    jq
+    bat
+    glow
+    w3m
+    eza
+    openssl
+    atool
+    p7zip
+    libcdio
+    odt2txt
+    catdoc
+    gnumeric
+    exiftool
+    chafa
+    mediainfo
+    ffmpegthumbnailer
+    poppler_utils
+  ];
   text = lib.readFile ./fzf-preview;
 
   meta = with lib; {
     description = "This is a fzf preview script to preview files in fzf";
     license = licenses.gpl3;
-    platforms = platforms.linux ++ platforms.darwin;
+    platforms = platforms.unix;
     maintainers = with maintainers; [ niksingh710 ];
     mainProgram = "fzf-preview";
   };
--- flake.nix
+++ flake.nix
@@ -4,11 +4,12 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems = import inputs.systems;
       perSystem = { pkgs, self', ... }: {
         packages.fzf-preview = pkgs.callPackage ./default.nix { };
         packages.default = self'.packages.fzf-preview;
